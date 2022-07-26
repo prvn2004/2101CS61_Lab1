21f6d81c5aac34cca942fd49367f494be709ced0
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2022-07-26 23:45:30 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    float a, b;
+    int c;
+    printf("Enter num1:");
+    scanf("%f", &a);
+    printf("Enter num2:");
+    scanf("%f", &b);
+    printf(" Select option: \n 1.Add    2.Substract    3.Multiply    4.Divide \n");
+    scanf("%d", &c);
+    switch (c)
+    {
+    case 1:
+        printf("%.2f", (a + b));
+        break;
+    return 0;
+}
