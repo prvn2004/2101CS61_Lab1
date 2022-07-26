98917d2272ce14d1ba249c4dcb704b87f0bcf51f
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2022-07-27 00:10:24 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -6,6 +6,18 @@ void swap(int *xp, int *yp)
     *xp = *yp; 
     *yp = temp; 
 } 
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+  
+        // Last i elements are already 
+        // in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
   
 void selectionSort(int arr[], int n) 
 { 
@@ -59,7 +71,8 @@ int main()
     printf("Before sorting array elements are - \n");  
     printArr(a, n);  
     //insert(a, n);  
-  selectionSort(a, n);
+  //selectionSort(a, n);
+    bubbleSort(arr, N);
     printf("\nAfter sorting array elements are - \n");    
     printArr(a, n);  
   
