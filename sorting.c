65265755ccd44405bdec10035058978e64c4ebe6
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2022-07-27 00:08:34 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -1,4 +1,33 @@
 #include <stdio.h>  
+
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of 
+    // unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        
+        // Find the minimum element in 
+        // unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+        if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element 
+        // with the first element 
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
   
 void insert(int a[], int n)
 {  
@@ -29,7 +58,8 @@ int main()
     int n = sizeof(a) / sizeof(a[0]);  
     printf("Before sorting array elements are - \n");  
     printArr(a, n);  
-    insert(a, n);  
+    //insert(a, n);  
+  selectionSort(a, n);
     printf("\nAfter sorting array elements are - \n");    
     printArr(a, n);  
   
